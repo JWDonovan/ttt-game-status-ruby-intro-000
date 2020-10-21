63e241dd5fdd1294b7ba31ddaeff08d7357851dd
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-21 01:59:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,6 +35,8 @@ def won?(board)
 
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return win_combination
+    elsif false
+      return false
     else
       nil
     end
